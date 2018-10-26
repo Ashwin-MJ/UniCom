80b1ba609e45680b22e739f55f8c3d50cafc1e92
Alice Ravier <2206245r@bo620-07u.dcs.gla.ac.uk>
2018-10-26 11:49:56 +0100

    Added models

--- student_feedback_project/student_feedback_app/models.py
+++ student_feedback_project/student_feedback_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+	username = models.CharField(max_length=40, primary_key=True)
+	password = models.CharField(max_length=40)
+	email = models.EmailField()
+	profile_picture = models.ImageField(null=True, blank=True)
+	score = models.IntegerField(default=0)
+	classes = models.ManyToManyField(Class, on_delete=models.CASCADE)
+
+class Class(models.Model):
+	subject = models.CharField(max_length=40, primary_key=True)
+	students = ManyToManyField(Student, on_delete=models.CASCADE)
+	lecturer = ForeignKey(Lecturer, on_delete=models.CASCADE)
+	leaderboard = ManyToManyField(Student, on_delete=models.CASCADE)
+
+class Lecturer(models.Model):
+	username = models.CharField(max_length=40, primary_key=True)
+	password = models.CharField(max_length=40)
+	email = models.EmailField()
+	profile_picture = models.ImageField(null=True, blank=True)
+	classes = models.ManyToManyField(Class, on_delete=models.CASCADE)
+
+class Feedback(models.Model):
+	category = models.CharField(max_length=100)
+	points = models.IntegerField(default=0)
+	lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
