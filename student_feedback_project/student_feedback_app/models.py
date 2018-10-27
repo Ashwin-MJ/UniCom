fca2824b5e9bcb2385d7a5fda9c4f6b599ccbcc5
Alice Ravier <aliceravier@yahoo.co.uk>
2018-10-27 14:31:39 +0100

    Make migrations for models

--- student_feedback_project/student_feedback_app/models.py
+++ student_feedback_project/student_feedback_app/models.py
@@ -7,13 +7,12 @@ class Student(models.Model):
 	email = models.EmailField()
 	profile_picture = models.ImageField(null=True, blank=True)
 	score = models.IntegerField(default=0)
-	classes = models.ManyToManyField(Class, on_delete=models.CASCADE)
+	classes = models.ManyToManyField('student_feedback_app.Class')
 
 class Class(models.Model):
 	subject = models.CharField(max_length=40, primary_key=True)
-	students = ManyToManyField(Student, on_delete=models.CASCADE)
-	lecturer = ForeignKey(Lecturer, on_delete=models.CASCADE)
-	leaderboard = ManyToManyField(Student, on_delete=models.CASCADE)
+	students = models.ManyToManyField('student_feedback_app.Student')
+	lecturer = models.ForeignKey('student_feedback_app.Lecturer', on_delete=models.CASCADE)
 
 class Lecturer(models.Model):
 	username = models.CharField(max_length=40, primary_key=True)
@@ -21,10 +20,10 @@ class Lecturer(models.Model):
 	password = models.CharField(max_length=40)
 	email = models.EmailField()
 	profile_picture = models.ImageField(null=True, blank=True)
-	classes = models.ManyToManyField(Class, on_delete=models.CASCADE)
+	classes = models.ManyToManyField(Class, related_name='taught_classes')
 
 class Feedback(models.Model):
 	category = models.CharField(max_length=100)
 	points = models.IntegerField(default=0)
-	lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
-	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+	lecturer = models.ForeignKey('student_feedback_app.Lecturer', on_delete=models.CASCADE)
+	student = models.ForeignKey('student_feedback_app.Student', on_delete=models.CASCADE)
