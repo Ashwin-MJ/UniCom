b66445045e54a3cbef2165c261abeab66fed550c
Adam  Christie <adamchristie@glaroam2-156-196.wireless.gla.ac.uk>
2018-11-21 12:27:35 +0000

    Have changed the course_token function.
    I have changed the function so that every time it runs, it checks that it doesn't already match any pre-existing course_tokens.

--- student_feedback_project/student_feedback_app/additional.py
+++ student_feedback_project/student_feedback_app/additional.py
@@ -1,6 +1,13 @@
 import string
 import random
-
+from .models import Course
 
 def course_code_generator(size = 7, chars= string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+    cT = ''.join(random.choice(chars) for _ in range(size))
+
+    for course in Course.objects.all():
+
+        if cT == course.course_token:
+            cT = course_code_generator()
+
+    return cT
