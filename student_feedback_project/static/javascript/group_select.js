ddead48563ed0c3d6cc408a42fe8bc95dd237af9
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2018-11-25 17:45:34 +0000

    Error if a lecturer tries to provide group feedback for less than 2 students
    A lecturer should only be able to provide group feedback if they select 2 or more students.
    If they select fewer than this, this function would be pointless.
    
    Issue #58

--- student_feedback_project/static/javascript/group_select.js
+++ student_feedback_project/static/javascript/group_select.js
@@ -3,20 +3,23 @@ $('.students').click(function(e) {
   $(this).toggleClass(addclass);
 });
 
-$('#add-group-feedback').click(function(){
+$('#group-feedback-form').submit(function(){
   var res=[];
   $('.students').each(function(){
     if($(this).hasClass('color')){
       res.push($(this).attr('id'))
     }
   })
+
   if(res.length < 2){
-    alert("Please select at least 2 students to provide group feedback.")
+    alert("Please select at least 2 students to provide group feedback.");
+    return false;
   }
   else{
     var jsonText = JSON.stringify(res);
     var cookieText = "students=" + jsonText + ';'
     document.cookie = cookieText;
+    return true;
   }
 
-})
+});
