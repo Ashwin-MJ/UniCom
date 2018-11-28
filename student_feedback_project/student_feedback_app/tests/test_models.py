c82c609bf27ccddf9033ff180912aabab5cdfc1d
2206245r <2206245r@student.gla.ac.uk>
2018-11-28 13:40:59 +0100

    Add test course description is correct, test course is added to student model and vice versa, add template for test date given is now

--- student_feedback_project/student_feedback_app/tests/test_models.py
+++ student_feedback_project/student_feedback_app/tests/test_models.py
@@ -1,5 +1,22 @@
 from student_feedback_app.models import Course
 from django.test import TestCase
+from populate.py import *
+
+class StudentTestCase(TestCase):
+    def setUp(self):
+        add_class("Systems Programming 3", "SP3", "Introduction to Systems Programming using C and C++")
+        add_student("Bob", "3015244", "Bob", "Bob@bob.bob", 0, "SP3")
+
+    def test_student_in_course(self):
+        course = Class.objects.get(course_code = "SP3")
+        student = StudentProfile.objects.get(student.student_id = "3015244")
+        self.assertEqual(course.students, student)
+
+    def test_course_in_student(self):
+        course = Class.objects.get(course_code = "SP3")
+        student = StudentProfile.objects.get(student.student_id = "3015244")
+        self.assertEqual(student.classes, course)
+
 
 class CourseTestCase(TestCase):
 
@@ -15,3 +32,11 @@ class CourseTestCase(TestCase):
         # The slug for the above course should be sp3
         systems = Course.objects.get(course_code="SP3")
         self.assertEqual(systems.subject_slug, "sp3")
+
+    def test_course_description_correct(self):
+        systems = Course.objects.get(course_code="SP3")
+        self.assertEqual(systems.course_description, "Introduction to Systems Programming using C and C++")
+
+class FeedbackTestCase(TestCase):
+    def setUp(self):
+    def test_date_given_is_now(self):
