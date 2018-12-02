2b50d967224aea354f491ae168b1c27e6de303ac
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2018-12-02 14:57:11 +0000

    Added tests for the feedback form
    Some basic tests have been added. More tests will need to be added, however this is a good
    starting point for this iteration.
    
    Issue #54

--- student_feedback_project/student_feedback_app/tests/test_add_feedback.py
+++ student_feedback_project/student_feedback_app/tests/test_add_feedback.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
-from student_feedback_app.models import Category,Message
+from student_feedback_app.models import *
+from student_feedback_app.forms import FeedbackForm
 
-# See test_models.py for an example test
-
-class FeedbackModelTestCase(TestCase):
+class CategoryAndMessageModelTestCase(TestCase):
 
     def setUp(self):
         # Set up a few categories and pre defined messages
@@ -36,12 +35,61 @@ class FeedbackModelTestCase(TestCase):
     def test_get_messages_by_category(self):
         speaking = Category.objects.get(name="Speaking")
         listening = Category.objects.get(name="Listening")
+        list_message = Message.objects.get(text="Thanks for paying attention today")
+        speak_message = Message.objects.get(text="Good speech today!")
 
         self.assertEqual(len(speaking.message_set.all()), 1)
         self.assertTrue(speaking.message_set.get(text="Good speech today!"))
+        self.assertNotIn(list_message,speaking.message_set.all())
 
         self.assertEqual(len(listening.message_set.all()), 1)
         self.assertTrue(listening.message_set.get(text="Thanks for paying attention today"))
+        self.assertNotIn(speak_message,listening.message_set.all())
+
+class IndividualFeedbackTestCase(TestCase):
+
+    def setUp(self):
+        stud_user = User(id_number="1234",username="Student",is_student=True)
+        lect_user = User(id_number="5678", username="Prof Lecturer",is_lecturer=True)
+        stud_user.save()
+        lect_user.save()
+
+        stud = StudentProfile(student=stud_user)
+        lect = LecturerProfile(lecturer=lect_user)
+        stud.save()
+        lect.save()
+
+        speaking = Category(name="Speaking")
+        speaking.save()
+
+        speak_message = Message(category=speaking,
+                                text="Good speech today!")
+        speak_message.save()
+
+        course = Course(subject="Systems Programming 3",
+                        course_description="Introduction to Systems Programming using C and C++",
+                        course_code="SP3")
+        course.save()
+
+    def test_feedback_form_true(self):
+        form_data = { "optional_message": "You made excellent points about ...",
+                        "category" : "Speaking",
+                        "points" : 5,
+                        "pre_defined_message" : "Good speech today!"}
+
+        fb_form = FeedbackForm(data=form_data)
+        self.assertTrue(fb_form.is_valid())
+
+    def test_feedback_form_false(self):
+        form_data = { "optional_message": "You were top of the class!",
+                        "category" : "Participation",
+                        "points" : 5,
+                        "pre_defined_message" : "Great marks in the quiz"}
+
+        fb_form = FeedbackForm(data=form_data)
+        # This test should fail as the category Participation and the pre defined message
+        # is not saved in the database
+        self.assertFalse(fb_form.is_valid())
 
 #class GroupFeedbackTestCase(TestCase):
 
