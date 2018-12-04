c3301607efb80aa08e53013987f852b33707e71a
2206245r <2206245r@student.gla.ac.uk>
2018-12-04 22:25:39 +0100

    start student home exception error test

--- student_feedback_project/student_feedback_app/tests/test_error_pages.py
+++ student_feedback_project/student_feedback_app/tests/test_error_pages.py
@@ -2,6 +2,9 @@ from django.test import Client
 from django.test import TestCase
 
 class StudentHomeErrorTestCase(TestCase):
+
+    def set_up(self):
+        authenticate(username='Ike', password='ikepass')
     
     def test_no_auth_page(self):
         c = Client()
@@ -12,4 +15,14 @@ class StudentHomeErrorTestCase(TestCase):
         c = Client()
         response = c.get('/student/home/')
         self.assertContains(response, "You are not logged in")
+
+    def test_exception_error_page(self):
+        c = Client()
+        print(c.login(username='Ike', password='ikepass'))
+        response = c.get('/student/home/')
+        #print(response.context)
+
+
+#TODO figure out how to pass info to context dict or otherwise get error
+#TODO get a user authenticated
 
