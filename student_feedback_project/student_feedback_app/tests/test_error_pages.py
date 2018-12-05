7de02f8828a75e05f2b90d4b89fccce31c257368
2206245r <2206245r@student.gla.ac.uk>
2018-12-05 15:26:05 +0100

    Add error page test for if user is lecturer should not be able to access student home + bug fix
    Fix bug that let lecturer access student/home by changing populate.py, setting is_student to false in add_lecturer
    
    Issue: #75

--- student_feedback_project/student_feedback_app/tests/test_error_pages.py
+++ student_feedback_project/student_feedback_app/tests/test_error_pages.py
@@ -1,10 +1,13 @@
 from django.test import Client
 from django.test import TestCase
+from populate import *
 
 class StudentHomeErrorTestCase(TestCase):
 
-    def set_up(self):
-        authenticate(username='Ike', password='ikepass')
+    def setUp(self):
+        add_course("Systems Programming 3", "SP3", "Introduction to Systems Programming using C and C++")
+        add_student("Bob", "3015244", "Bob@bob.bob", "Bob", 0, ["SP3"])
+        add_lecturer("Wolf", "00001", "star", "wolf@star.com", ["SP3"])
     
     def test_no_auth_page(self):
         c = Client()
@@ -16,14 +19,22 @@ class StudentHomeErrorTestCase(TestCase):
         response = c.get('/student/home/')
         self.assertContains(response, "You are not logged in")
 
+    def test_lecturer_wrong_auth_page(self):
+        c = Client()
+        c.login(username="00001", password="star")
+        response = c.get('/student/home/')
+        self.assertEqual(response.templates[0].name, "student_feedback_app/error_page.html")
+        c.logout()
+
     def test_exception_error_page(self):
         c = Client()
-        c.login(username='Ike', password='ikepass')
+        c.login(username="3015244", password="Bob")
         response = c.get('/student/home/')
+        c.logout()
         #print(response.content)
         #print(response.context)
 
 
 #TODO figure out how to pass info to context dict or otherwise get error
-#TODO get a user authenticated
+
 
