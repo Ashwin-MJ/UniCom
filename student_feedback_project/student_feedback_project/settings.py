dc33b26e0b9b2d9b937f2fdc123738fe0002799f
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2019-01-09 12:15:45 +0000

    Front end updates

--- student_feedback_project/student_feedback_project/settings.py
+++ student_feedback_project/student_feedback_project/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'dal', # Django Autocomplete Light
-    'dal_select2', 
+    'dal_select2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'student_feedback_app',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
