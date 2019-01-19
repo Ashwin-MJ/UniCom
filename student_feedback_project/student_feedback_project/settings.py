b87eb04dff671b056da7dd4a6ed49ef7e6267d71
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2019-01-19 11:13:30 +0000

    Fix static files issue

--- student_feedback_project/student_feedback_project/settings.py
+++ student_feedback_project/student_feedback_project/settings.py
@@ -20,7 +20,6 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -149,4 +148,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 ADMINS = [('Default email', 'lect.acc.unicom@gmail.com'),]
-
