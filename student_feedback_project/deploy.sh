c10d9e02059eac1037b3bb2c02b7645713fa695c
Jonas <jonciukelis@gmail.com>
2019-01-23 12:03:13 +0000

    PythonAnywhere deployment script added

---
+++ student_feedback_project/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+eval ". ~/home/feedbackapp/unicom"
+eval "pip3 install -r requirements.txt"
+rm db.sqlite3
+eval "python3 manage.py migrate --run-syncdb"
+eval "python3 populate.py"
+touch ./student_feedback_app/__init__.py
+LINE="STATIC_ROOT = os.path.join(BASE_DIR, 'static')"
+FILE=./student_feedback_project/settings.py
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+yes yes | eval "python3 manage.py collectstatic"
+
