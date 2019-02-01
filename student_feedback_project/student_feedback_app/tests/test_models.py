b3eeeb71dd602a71455d39d2e71e02770f327f13
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2019-02-01 19:46:50 +0000

    Updated population script
    The population script has been updated to include Dr Wong's list of categories and pre-defined
    messages.
    I also had to update test_models.py as tests were failing due to a change I made in populate.py
    to make the database population a bit nicer. The method add_message() now takes a list as the
    second parameter.
    
    Issue #114

--- student_feedback_project/student_feedback_app/tests/test_models.py
+++ student_feedback_project/student_feedback_app/tests/test_models.py
@@ -5,7 +5,7 @@ from populate import *
 # Ensure all test methods begin with test_..... otherwise they will not run
 
 class StudentTestCase(TestCase):
-    
+
     def setUp(self):
         add_course("Systems Programming 3", "SP3", "Introduction to Systems Programming using C and C++")
         add_student("Bob", "3015244", "Bob@bob.bob", "Bob", 0, ["SP3"])
@@ -32,21 +32,21 @@ class CourseTestCase(TestCase):
         # The slug for the above course should be sp3
         systems = Course.objects.get(course_code="SP3")
         self.assertEqual(systems.subject_slug, "sp3")
-        
+
     def test_course_description_correct(self):
         systems = Course.objects.get(course_code="SP3")
         self.assertEqual(systems.course_description, "Introduction to Systems Programming using C and C++")
 
 class FeedbackTestCase(TestCase):
-    
+
     def setUp(self):
         add_course("Systems Programming 3", "SP3", "Introduction to Systems Programming using C and C++")
         add_lecturer("Wolf", "00001", "star", "wolf@star.com", ["SP3"])
         add_student("Bob", "3015244", "Bob@bob.bob", "Bob", 0, ["SP3"])
         add_category("writing")
-        add_message("writing", "wrote well")
-        add_feedback(1, "writing", 4, "00001", "3015244", "SP3", "wrote well", "good explanation of pointers") 
-                    
+        add_message("writing", ["wrote well"])
+        add_feedback(1, "writing", 4, "00001", "3015244", "SP3", "wrote well", "good explanation of pointers")
+
     def test_fb_correct_optional_message(self):
         fb = Feedback.objects.get(feedback_id=1)
         self.assertEqual(fb.optional_message, "good explanation of pointers")
@@ -98,7 +98,7 @@ class MessageTestCase(TestCase):
 
     def setUp(self):
         add_category("listening")
-        add_message("listening", "listened well")
+        add_message("listening", ["listened well"])
 
     def test_message_correct(self):
         message = Message.objects.get(category="listening")
@@ -112,8 +112,3 @@ class MessageTestCase(TestCase):
     def test_default_message(self):
         #TODO
         self.assertTrue(True)
-
-
-
-        
-
