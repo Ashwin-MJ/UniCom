b06bdd5207fb14c627992d5bf216795f0f1f0ff7
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2019-02-16 15:49:05 +0000

    Tidy up customise.js file
    Issue #100

--- student_feedback_project/static/javascript/customise.js
+++ student_feedback_project/static/javascript/customise.js
@@ -14,7 +14,11 @@ function getCookie(cname) {
   return "";
 }
 
+
 $('.delete-cat-icon').click(function(e) {
+  // This allows a category to be deleted
+  // An issue is that when deleted, all feedback associated with that category is also deleted
+  // This cannot be avoided, but the option should still be there. So a warning comes up
   var result = confirm("If you delete this category then every feedback you have given with this category will be deleted. Do you want to continue?");
   if(result){
     var cat_id = this.id;
@@ -42,21 +46,17 @@ $('.delete-cat-icon').click(function(e) {
 });
 
 $('.edit-cat-icon').click(function(e) {
+  // This allows a category to be edited (text and colour)
   var cat_id = this.id;
   var cat_name = $(this).parent().find("b").html();
-
   $('.modal-edit-cat-header').html("Edit \"" + cat_name + "\"");
-
-  cat_name = cat_name.replace("&amp;", "&")
-
+  cat_name = cat_name.replace("&amp;", "&"); // Had to include this due to JSON issue
   document.getElementById("id_name").value = cat_name;
   document.getElementById("edit_cat_colour").value = "#009999";
 
   $('.submit-edit-cat-form').on('click', function(){
-
     var new_name = $('#id_name').val();
     var new_colour = $('#edit_cat_colour').val()
-
     var data = {
              "name": new_name,
              "colour": new_colour
@@ -82,22 +82,17 @@ $('.edit-cat-icon').click(function(e) {
         success: function() {
         },
     });
-
-    location.reload()
-
+    location.reload() // Not sure why, but I had to move this out of the 'success' as it occasionally did not work
   });
-
 });
 
 $('.create-cat-icon').click(function(e) {
-
+  // Pre set colour field to the default
+  // This is needed if the submit is clicked without choosing a colour
   document.getElementById("new_cat_colour").value = "#009999";
-
   $('.submit-add-cat-form').on('click', function(){
-
     var new_name = $('#id_new_name').val();
     var new_colour = $('#new_cat_colour').val()
-
     var data = {
              "name": new_name,
              "colour": new_colour
@@ -123,9 +118,7 @@ $('.create-cat-icon').click(function(e) {
         success: function() {
         },
     });
-
-    location.reload()
-
+    location.reload() // Same issue as mentioned above
   });
 
 });
@@ -133,8 +126,8 @@ $('.create-cat-icon').click(function(e) {
 
 
 $('.category').on('click', function(){
+  // Dynamically display messages based on category selected
   var cat_id = $(this).find("i").attr('id');
-
   var csrftoken = getCookie("csrftoken");
   function csrfSafeMethod(method) {
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
@@ -152,6 +145,9 @@ $('.category').on('click', function(){
       type: 'GET',
       contentType: 'application/json',
       success: function(result) {
+        // The difficulty with this was that the context dictionary cannot be accessed in this
+        // separate javascript file. So call a method in the HTML template which retrieves data
+        // And sends data back to file in method updateHtml()
         displayMessages(result,cat_id)
       },
   });
@@ -159,8 +155,9 @@ $('.category').on('click', function(){
 });
 
 function updateHtml(colour,message_set,cat_name,cat_id) {
+  // Updates the cards shown in the message card. This allows the messages shown to change dependent
+  // on which category is selected
   var cat_messages = "";
-
   for (var retrieved_message in message_set){
     for (var message_id in all_messages){
       if (message_set[retrieved_message] == message_id){
@@ -173,14 +170,14 @@ function updateHtml(colour,message_set,cat_name,cat_id) {
     }
   }
   $('.cat-title').html(cat_name);
-  $('.cat-title').attr("id",cat_id)
-  $('#add-mess-icon').html("add_circle_outline");
+  $('.cat-title').attr("id",cat_id); // Need to access cat_id in another method so set the id for easy retrieval
+  $('#add-mess-icon').html("add_circle_outline"); // Only show add_message button if a category is selected
   $('.messages').html(cat_messages);
-
 }
 
 $('#all-messages').on('click','.delete-mess-icon', function() {
-  console.log("Here")
+  // Same issue as mentioned above. When a message is deleted, all feedback associated with that message
+  // will also be deleted. Give a warning for this
   var result = confirm("If you delete this message then every feedback you have given with this message will be deleted. Do you want to continue?");
   if(result){
     var mess_id = this.id;
@@ -208,21 +205,18 @@ $('#all-messages').on('click','.delete-mess-icon', function() {
 });
 
 $('.create-mess-icon').on('click',function(e) {
-
+  // Dynamically update modal for this option
+  // Then send POST request from here
   var cat_name = $('.cat-title').html();
   var cat_id = $('.cat-title').attr('id');;
 
   $('.modal-add-mess-header').html("Add a new message for \"" + cat_name + "\"");
-
   $('.submit-add-mess-form').on('click', function(){
-
     var text = $('#id_text').val();
-
     var data = {
              "text": text,
              "category": cat_id
            }
-
     var csrftoken = getCookie("csrftoken");
     function csrfSafeMethod(method) {
       return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
@@ -243,9 +237,7 @@ $('.create-mess-icon').on('click',function(e) {
         success: function() {
         },
     });
-
-    location.reload()
-
+    location.reload() // Same issue as mentioned above
   });
 
 });
